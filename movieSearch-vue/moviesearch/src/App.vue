3de519b240ea3deec2cad5cd4dc9c7e82ec562f4
Johan Bijlsma <johan.bijlsma24@gmail.com>
2023-01-06 16:58:38 +0100

    started inmplementing a version within Vue3

--- movieSearch-vue/moviesearch/src/App.vue
+++ movieSearch-vue/moviesearch/src/App.vue
@@ -1,25 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import MovieSearchVue from "./components/MovieSearch.vue";
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>Johan's Movie search</h1>
+    <p class="subheading">Powered by the The MovieDB</p>
   </header>
-
   <main>
-    <TheWelcome />
+    <div class="wrapper"></div>
+
+    <MovieSearchVue />
   </main>
 </template>
 
@@ -36,15 +27,15 @@ header {
 @media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    min-height: 80px;
+    /* place-items: center;
+    padding-right: calc(var(--section-gap) / 2); */
   }
 
   .logo {
     margin: 0 2rem 0 0;
   }
-
-  header .wrapper {
+  .wrapper {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
---
+++ movieSearch-vue/moviesearch/src/components/MovieSearch.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+const placeholder = "Type here to start searching...";
+const suggestions = [
+  "Star Wars",
+  "Spiderman",
+  "James Bond",
+  "Spongebob Squarepants",
+  "The Godfather",
+];
+</script>
+
+<template>
+  <section>
+    <p class="intro">
+      To start using this site, please enter a search query to look for Movie
+      titles.
+    </p>
+    <input
+      type="search"
+      name="movieSearch"
+      id="movieSearch"
+      :placeholder="placeholder"
+      className="searchMovie"
+    />
+    <div class="suggestions">
+      How about:
+      <template v-for="(idea, index) in suggestions" :key="index">
+        <span>{{ idea }}</span>
+      </template>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  display: grid;
+  grid-template-areas: "intro intro intro" "search search search" "sugestions sugestions sugestions" "result result result";
+  grid-template-rows: auto 100px 100px 1fr;
+  background: linear-gradient(
+    45deg,
+    var(--primary-20),
+    var(--primary) 15%,
+    var(--secondary),
+    var(--secondary-20)
+  );
+  min-height: 80vh;
+  min-width: 80vw;
+  transition: box-shadow 200ms ease-in-out;
+}
+
+section:focus-within {
+  background: linear-gradient(45deg, var(--primary-20), var(--secondary-20));
+}
+input[type="search"] {
+  margin: 1.4rem 2rem;
+  font-size: 1.6rem;
+  line-height: 2rem;
+  padding: 0.3rem;
+  border-radius: 10px;
+  max-height: 2.4rem;
+  box-shadow: 0px 0px 1px 3px var(--primary-20),
+    0px 0px 1px 3px var(--primary-20), 0px 0px 1px 3px var(--primary-20);
+  transition: box-shadow 200ms ease-in-out;
+  grid-area: search;
+}
+input[type="search"]:focus,
+input[type="search"]:focus-within,
+input[type="search"]:focus-visible {
+  box-shadow: 0px 0px 11px 8px var(--primary), 0px 0px 11px 8px var(--primary),
+    0px 0px 11px 8px var(--primary);
+  transition: all 200ms ease-in-out;
+  margin: 1.4rem 5vw;
+}
+
+p.intro {
+  grid-area: intro;
+  font-size: 1.6rem;
+  text-align: center;
+  margin-top: 1.2rem;
+  position: relative;
+  z-index: 10;
+  background-blend-mode: multiply;
+  text-shadow: 0 0 white;
+}
+.suggestions {
+  grid-area: sugestions;
+  font-size: 1.6rem;
+  text-align: center;
+  margin-top: 1.2rem;
+  text-shadow: 0 0 white;
+}
+</style>
