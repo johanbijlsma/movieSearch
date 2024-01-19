b1655bbef93640c74bc65fa7200c197c8652dbe9
Johan Bijlsma <johan.bijlsma24@gmail.com>
2024-01-19 07:58:47 +0100

    added a transition

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -3,5 +3,27 @@
 </script>
 <template>
 	<router-link to="/"> Back to search </router-link>
-	<MovieDetail />
+	<Transition name="slide-fade">
+		<MovieDetail />
+	</Transition>
 </template>
+
+<style>
+	/*
+  Enter and leave animations can use different
+  durations and timing functions.
+*/
+	.slide-fade-enter-active {
+		transition: all 0.3s ease-out;
+	}
+
+	.slide-fade-leave-active {
+		transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+
+	.slide-fade-enter-from,
+	.slide-fade-leave-to {
+		transform: translateX(20px);
+		opacity: 0;
+	}
+</style>
